642bf17d620bd53e0570b9ebffe2a395c7cafc7f
LisaEfrianti <49120337+LisaEfrianti@users.noreply.github.com>
2019-10-16 21:27:13 +0800

    Create balok.py

---
+++ balok.py
@@ -0,0 +1,16 @@
+import math
+
+def LuasBalok(p, l, t)
+    return 2*(p*l+p*t+l*t)
+
+def volume(p, l, t)
+    return p*l*t
+    
+def panjang(v, l, t)
+    return v/l/t
+    
+def lebar(v, p, t)
+    return v/p/t
+    
+def tinggi(v, p, l)
+    return v/p/l
